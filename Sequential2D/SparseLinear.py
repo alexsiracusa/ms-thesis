5abb73d5a410f11143a96d1ee2987743f3cb4b28
alexsiracusa <alexander.siracusa@gmail.com>
2025-04-04 11:04:29 -0400

    update

--- Sequential2D/SparseLinear.py
+++ Sequential2D/SparseLinear.py
@@ -16,8 +16,7 @@ class SparseLinear(torch.nn.Module):
         self.out_features = out_features
 
         weight = torch.empty((out_features, in_features)).to_sparse_csr()
-        # self.weight = torch.nn.Parameter(weight)
-        self.register_buffer("weight", weight)
+        self.weight = torch.nn.Parameter(weight)
 
         if bias:
             self.bias = torch.nn.Parameter(torch.empty(out_features))
@@ -27,10 +26,12 @@ class SparseLinear(torch.nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self) -> None:
+        device = self.weight.device
         weight = torch.empty((self.out_features, self.in_features))
         init.kaiming_uniform_(weight, a=math.sqrt(5))
         weight = weight * self.mask
         self.weight = torch.nn.Parameter(weight.to_sparse_csr())
+        self.weight.to(device)
 
         if self.bias is not None:
             fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
@@ -52,17 +53,17 @@ class SparseLinear(torch.nn.Module):
         mask = random_boolean_tensor(out_features, in_features, int(percent * total_elements))
         return SparseLinear(in_features, out_features, bias=bias, mask=mask)
 
-    def to(self, device):
-        print('custom to')
-        """Ensure sparse CSR components are properly moved."""
-        new_model = super().to(device)  # Move dense tensors like bias
-        new_model.weight = torch.sparse_csr_tensor(
-            self.weight.crow_indices().to(device),
-            self.weight.col_indices().to(device),
-            self.weight.values().to(device),
-            self.weight.shape,
-            device=device,
-        )
-        return new_model
+    # def to(self, device):
+    #     print('custom to')
+    #     """Ensure sparse CSR components are properly moved."""
+    #     new_model = super().to(device)  # Move dense tensors like bias
+    #     new_model.weight = torch.sparse_csr_tensor(
+    #         self.weight.crow_indices().to(device),
+    #         self.weight.col_indices().to(device),
+    #         self.weight.values().to(device),
+    #         self.weight.shape,
+    #         device=device,
+    #     )
+    #     return new_model
 
 

00b2deb149f1f8653fa6384699f8561234e84f00
alexsiracusa <alexander.siracusa@gmail.com>
2025-04-04 11:07:51 -0400

    update

--- Sequential2D/SparseLinear.py
+++ Sequential2D/SparseLinear.py
@@ -26,24 +26,15 @@ class SparseLinear(torch.nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self) -> None:
-        device = self.weight.device
         weight = torch.empty((self.out_features, self.in_features))
         init.kaiming_uniform_(weight, a=math.sqrt(5))
         weight = weight * self.mask
         self.weight = torch.nn.Parameter(weight.to_sparse_csr())
-        self.weight.to(device)
 
         if self.bias is not None:
             fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
             bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
             init.uniform_(self.bias, -bound, bound)
-
-        print('Reset parameters')
-        print('weight', self.weight.device)
-        print('row', self.weight.crow_indices().device)
-        print('col', self.weight.col_indices().device)
-        print('values', self.weight.values().device)
-        print('bias', self.bias.device)
 
     def forward(self, X):
         print('Forward')
@@ -60,6 +51,17 @@ class SparseLinear(torch.nn.Module):
         total_elements = in_features * out_features
         mask = random_boolean_tensor(out_features, in_features, int(percent * total_elements))
         return SparseLinear(in_features, out_features, bias=bias, mask=mask)
+
+    def to(self, *args, **kwargs):
+        print("to device*********")
+        device = kwargs.get("device", None)
+        if device is None and len(args) > 0:
+            device = args[0]
+
+        self.mask = self.mask.to(device)
+        self.reset_parameters()  # Recreate self.weight on new device
+
+        return super().to(*args, **kwargs)
 
     # def to(self, device):
     #     print('custom to')

ed6fef2fd919dc46ed364402f720992eb814b282
alexsiracusa <alexander.siracusa@gmail.com>
2025-04-05 16:34:59 -0400

    test

---
+++ Playground/sparse_csr.py
@@ -0,0 +1,48 @@
+from util import random_boolean_tensor
+from torch import nn
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import torch
+
+device = torch.device('cuda')
+tensor_dim = 2000
+
+sparsity_values = np.arange(0, 1.1, 0.1)
+dense_times = []
+sparse_times = []
+
+linear = nn.Linear(tensor_dim, tensor_dim, bias=False)
+full_tensor = linear.weight
+
+for sparsity in sparsity_values:
+    mask = random_boolean_tensor(tensor_dim, tensor_dim, int(tensor_dim**2 * sparsity))
+
+    tensor = (full_tensor * mask)
+    dense = tensor.to_dense().to(device)
+    sparse = tensor.to_sparse_csr().to(device)
+    X = torch.normal(0, 1, size=(100, tensor_dim)).to(device)
+
+    start = time.time() # START TIMER
+    _ = F.linear(X, dense, None)
+    dense_times.append(time.time() - start) # END TIMER
+
+    start = time.time()  # START TIMER
+    _ = F.linear(X, sparse, None)
+    sparse_times.append(time.time() - start)  # END TIMER
+
+
+plt.figure(figsize=(10, 5))
+plt.plot(sparsity_values, dense_times, label='Dense', color='red')
+# plt.plot(sparsity_values, sparse_times, label='Sparse', color='blue')
+
+# Labels and title
+plt.xlabel('Sparsity')
+plt.ylabel('Time')
+plt.title('Dot Product Time vs. Sparsity')
+plt.legend()
+
+# Save as PNG
+plt.savefig('dot_times.png', dpi=300)
+plt.show()

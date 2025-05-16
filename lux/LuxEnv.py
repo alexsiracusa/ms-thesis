21209af4c0244f55b9823df8f063a93941b69734
alexsiracusa <alexander.siracusa@gmail.com>
2025-05-15 22:45:09 -0400

    update imports

--- lux/LuxEnv.py
+++ lux/LuxEnv.py
@@ -1,8 +1,7 @@
 from typing import Optional
 import numpy as np
 import gymnasium as gym
-from lux_design_s3.src.luxai_s3.env import LuxAIS3Env
-
+from lux.lux_design_s3.src.luxai_s3 import LuxAIS3Env
 
 class LuxEnv(gym.Env):
 

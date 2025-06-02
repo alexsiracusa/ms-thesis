0b78c20c9aac27050621d7a54f98f8d986df2add
alexsiracusa <alexander.siracusa@gmail.com>
2025-06-02 17:59:42 -0400

    refactor

--- lux/models/model.py
+++ lux/models/model.py
@@ -1,91 +1,11 @@
 from torch.utils.data import DataLoader
 from build_sequential2d import build_sequential2d
 from lux.util import load_action_dataset, action_collate_fn
+from lux.models.sizes import input_sizes, hidden_sizes, output_sizes
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
-
-# T is the number of teams (default is 2)          2
-# N is the max number of units per team            16
-# W, H are the width and height of the map         24x24
-# R is the max number of relic nodes               6
-
-# Total = 3619
-input_sizes = [
-            # SHAPE                    DIMS     NAME                    TYPE
-    64,     # (T, N, 2) = 2 * 16 * 2  = 64       units position          coordinates
-    32,     # (T, N, 1) = 2 * 16 * 1  = 32       units energy            continuous
-    32,     # (T, N)    = 2 * 16      = 32       units_mask              binary
-    576,    # (W, H)    = 24 * 24     = 576      sensor_mask             binary
-    576,    # (W, H)    = 24 * 24     = 576      map_features energy     continuous
-
-    2304,   # (W, H, 4) = 24 * 24 * 4 = 2304     map_features tile_type  categorical
-            # UNKNOWN = -1
-            # EMPTY_TILE = 0
-            # NEBULA_TILE = 1
-            # ASTEROID_TILE = 2
-
-    6,      # (R)       = 6           = 6        relic_nodes_mask        binary
-    12,     # (R, 2)    = 6 * 2       = 12       relic_nodes             coordinates
-    2,      # (T)       = 2           = 2        team_points             continuous
-    2,      # (T)       = 2           = 2        team_wins               continuous
-    1,      # (1)       = 1           = 1        steps                   continuous
-    1,      # (1)       = 1           = 1        match_steps             continuous
-    1,      # (1)       = 1           = 1        remainingOverageTime    continuous
-
-    1,      # (1)       = 1           = 1        max_units               CONSTANT = 16
-    1,      # (1)       = 1           = 1        match_count_per_episode continuous
-    1,      # (1)       = 1           = 1        max_steps_in_match      continuous
-    1,      # (1)       = 1           = 1        map_height              CONSTANT = 24
-    1,      # (1)       = 1           = 1        map_width               CONSTANT = 24
-    1,      # (1)       = 1           = 1        num_teams               CONSTANT = 2
-    1,      # (1)       = 1           = 1        unit_move_cost          continuous
-    1,      # (1)       = 1           = 1        unit_sap_cost           continuous
-    1,      # (1)       = 1           = 1        unit_sap_range          continuous
-    1,      # (1)       = 1           = 1        unit_sensor_range       continuous
-]
-
-# total = 4620
-hidden_sizes = [
-    2304,   # spaces to remember map features
-    576,
-    576,
-    64,     # spaces to remember unit information
-    64,
-    64,
-    12,     # space to remember relic nodes
-
-    144,    # Generic extra spaces for the model to figure out
-    144,
-    144,
-    144,
-    64,
-    64,
-    64,
-    64,
-    64,
-    64,
-]
-
-# 5 possible actions
-#   DO_NOTHING = 0
-#   MOVE_UP = 1
-#   MOVE_RIGHT = 2
-#   MOVE DOWN = 3
-#   MOVE_LEFT = 4
-#   SAP = 5
-
-# With two additional parameters for the SAP action
-#   DELTA_X = int
-#   DELTA_Y = int
-output_sizes = [
-    128     # (N, 8)    = 16 * 8    = 128       actions                 categorical
-]
-
-print(f'Input Total:  {sum(input_sizes)}')
-print(f'Hidden Total: {sum(hidden_sizes)}')
 
 
 # TRAINING
@@ -138,7 +58,7 @@ for epoch in range(num_epochs):
 
     for batch_obs, batch_act, batch_sap, lengths in dataloader:
         start = 100
-        max_seq_len = 20
+        max_seq_len = 1
         batch_size = batch_obs.shape[0]
 
         batch_obs = batch_obs.to(device)[:, start:start+max_seq_len]  # (batch_size, max_seq_len, input_dim)
@@ -151,7 +71,6 @@ for epoch in range(num_epochs):
 
         num_ce = batch_size * max_seq_len * 16
         num_mse = batch_size * max_seq_len * 32
-        # print(batch_act[0][0][0], output[0][0][:6])
         print(f"CE Loss: {ce_loss.item() / num_ce:.4f} MSE Loss: {mse_loss.item() / num_mse:.4f}")
 
         optimizer.zero_grad()
---
+++ lux/models/sizes.py
@@ -0,0 +1,81 @@
+
+# T is the number of teams (default is 2)          2
+# N is the max number of units per team            16
+# W, H are the width and height of the map         24x24
+# R is the max number of relic nodes               6
+
+# Total = 3619
+input_sizes = [
+            # SHAPE                    DIMS     NAME                    TYPE
+    64,     # (T, N, 2) = 2 * 16 * 2  = 64       units position          coordinates
+    32,     # (T, N, 1) = 2 * 16 * 1  = 32       units energy            continuous
+    32,     # (T, N)    = 2 * 16      = 32       units_mask              binary
+    576,    # (W, H)    = 24 * 24     = 576      sensor_mask             binary
+    576,    # (W, H)    = 24 * 24     = 576      map_features energy     continuous
+
+    2304,   # (W, H, 4) = 24 * 24 * 4 = 2304     map_features tile_type  categorical
+            # UNKNOWN = -1
+            # EMPTY_TILE = 0
+            # NEBULA_TILE = 1
+            # ASTEROID_TILE = 2
+
+    6,      # (R)       = 6           = 6        relic_nodes_mask        binary
+    12,     # (R, 2)    = 6 * 2       = 12       relic_nodes             coordinates
+    2,      # (T)       = 2           = 2        team_points             continuous
+    2,      # (T)       = 2           = 2        team_wins               continuous
+    1,      # (1)       = 1           = 1        steps                   continuous
+    1,      # (1)       = 1           = 1        match_steps             continuous
+    1,      # (1)       = 1           = 1        remainingOverageTime    continuous
+
+    1,      # (1)       = 1           = 1        max_units               CONSTANT = 16
+    1,      # (1)       = 1           = 1        match_count_per_episode continuous
+    1,      # (1)       = 1           = 1        max_steps_in_match      continuous
+    1,      # (1)       = 1           = 1        map_height              CONSTANT = 24
+    1,      # (1)       = 1           = 1        map_width               CONSTANT = 24
+    1,      # (1)       = 1           = 1        num_teams               CONSTANT = 2
+    1,      # (1)       = 1           = 1        unit_move_cost          continuous
+    1,      # (1)       = 1           = 1        unit_sap_cost           continuous
+    1,      # (1)       = 1           = 1        unit_sap_range          continuous
+    1,      # (1)       = 1           = 1        unit_sensor_range       continuous
+]
+
+# total = 4620
+hidden_sizes = [
+    2304,   # spaces to remember map features
+    576,
+    576,
+    64,     # spaces to remember unit information
+    64,
+    64,
+    12,     # space to remember relic nodes
+
+    144,    # Generic extra spaces for the model to figure out
+    144,
+    144,
+    144,
+    64,
+    64,
+    64,
+    64,
+    64,
+    64,
+]
+
+# 5 possible actions
+#   DO_NOTHING = 0
+#   MOVE_UP = 1
+#   MOVE_RIGHT = 2
+#   MOVE DOWN = 3
+#   MOVE_LEFT = 4
+#   SAP = 5
+
+# With two additional parameters for the SAP action
+#   DELTA_X = int
+#   DELTA_Y = int
+output_sizes = [
+    128     # (N, 8)    = 16 * 8    = 128       actions                 categorical
+]
+
+if __name__ == '__main__':
+    print(f'Input Total:  {sum(input_sizes)}')
+    print(f'Hidden Total: {sum(hidden_sizes)}')

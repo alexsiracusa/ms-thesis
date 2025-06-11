015977f0d89b3a5131f166976ab951d580ba42c6
alexsiracusa <alexander.siracusa@gmail.com>
2025-06-11 16:59:21 -0400

    added documentation to mask.py

--- Sequential2D/util/mask.py
+++ Sequential2D/util/mask.py
@@ -1,6 +1,15 @@
 import torch
 
 
+"""
+Args:
+    rows (int): The number of rows in the resulting tensor
+    cols (int): The number of columns in the resulting tensor
+    percent (float): The percent of 1's in the resulting tensor mask, the rest being 0's
+    
+Return:
+    tensor (rows, cols): A tensor with the specified size and sparsity
+"""
 def random_mask(rows, cols, percent):
     total_elements = rows * cols
     num_true = int(total_elements * percent)
@@ -11,6 +20,29 @@ def random_mask(rows, cols, percent):
     return shuffled_values.view(rows, cols)
 
 
+"""
+Args:
+    row_blocks (N): The sizes for each row of blocks
+    col_blocks (M): The sizes for each column of blocks
+    densities (N, M): The density of each [i, j] block
+    
+Returns:
+    tensor (n_rows, n_cols): A tensor with varying densities per block where:
+        - n_rows = sum(row_blocks) 
+        - n_cols = sum(col_blocks)
+        
+Example:
+    row_blocks = [1, 2]
+    col_blocks = [1, 2, 3]
+    densities = [[A, B, C]
+                 [D, E, F]]
+
+    tensor = [[a, b, b, c, c, c]
+              [d, e, e, f, f, f]
+              [d, e, e, f, f, f]]
+
+    such that A% of the entries labeled 'a' in `tensor` are 1's, and the rest are 0's, etc.
+"""
 def variable_mask(row_blocks, col_blocks, densities):
     mask = torch.zeros((sum(row_blocks), sum(col_blocks)))
 

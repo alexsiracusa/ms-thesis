ba4de7e5490f8be8725116c0c142130d592c46b5
alexsiracusa <alexander.siracusa@gmail.com>
2025-10-11 23:22:32 -0400

    new sweep id

--- mnist/run_sweep.py
+++ mnist/run_sweep.py
@@ -3,7 +3,7 @@ from mnist.wandb_project import project_name
 from mnist.train_model import train_model
 
 wandb.agent(
-    sweep_id='l1r0aps1',
+    sweep_id='z5t9nxdv',
     function=train_model,
     project=project_name,
 )

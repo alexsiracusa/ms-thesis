ce2b9078b5f268e86bb182eb188e4239fbee3d13
alexsiracusa <alexander.siracusa@gmail.com>
2025-10-16 17:43:47 -0400

    idk

--- cifar10/sweep/slurm.sh
+++ cifar10/sweep/slurm.sh
@@ -18,16 +18,32 @@ module load git
 module load python/3.11.12
 
 
+# Clone git repo
+REPO_URL="https://github.com/alexsiracusa/ms-thesis.git"
 REPO_DIR="$HOME/ms-thesis"
-if [ -d "$REPO_DIR" ]; then
+LOCK_FILE="$HOME/.ms-thesis-git-lock"
+
+# Acquire exclusive lock to prevent concurrent git operations
+exec 9>"$LOCK_FILE"
+flock -x 9
+
+if [ -d "$REPO_DIR/.git" ]; then
     cd "$REPO_DIR"
-    git pull
+    echo "Updating existing repository..."
+    git fetch origin main
+    git reset --hard origin/main
+    git clean -fdx
 else
-    git clone https://github.com/alexsiracusa/ms-thesis.git "$REPO_DIR"
+    echo "Cloning fresh repository..."
+    git clone --depth=1 "$REPO_URL" "$REPO_DIR"
     cd "$REPO_DIR"
 fi
 
+# Release lock automatically when script ends
+flock -u 9
 
+
+# Create python env
 ENV_DIR="$REPO_DIR/env"
 python3 -m venv "$ENV_DIR"
 source "$ENV_DIR/bin/activate"
@@ -40,7 +56,9 @@ poetry install
 which python
 python -m pip list
 
-SWEEP_ID="ikg8uvsq"
+
+# Run job
+SWEEP_ID="yyawbxoz"
 KEY=""
 
 source "$ENV_DIR/bin/activate"
